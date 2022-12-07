68fb80d825f844e6c2bf69cb4b65cf13e6d250b4
johnsimpson-phsa <90870225+johnsimpson-phsa@users.noreply.github.com>
2022-12-07 06:40:16 -0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ClinicalOfficeMpageModule} from "@clinicaloffice/clinical-office-mpage";
+import {MaterialModule} from "@clinicaloffice/clinical-office-mpage";
+import {ErrorHandlerService} from "@clinicaloffice/clinical-office-mpage";
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClinicalOfficeMpageModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: ErrorHandlerService}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
